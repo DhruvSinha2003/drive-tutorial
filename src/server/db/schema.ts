f91d9cce7b4506f97e996af8bd82a2f076204556
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-02-13 01:00:52 +0530

    sleep time

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,2 +1,13 @@
-import { int, singlestoreTable, text } from "drizzle-orm/singlestore-core";
+import { index, int, singlestoreTable, text } from "drizzle-orm/singlestore-core";
 
+export const files = singlestoreTable("files_table", {
+  id: int("id").primaryKey().autoIncrement(),
+  name: text("name"),
+  url: text("url"),
+  parent: int("parent"),
+  size: int("size"),
+},(t) => {
+  return [
+    index("parent_index").on(t.parent)
+  ]
+});
