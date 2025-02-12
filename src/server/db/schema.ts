abb76a417fdbd8a2c656d256a56e514659e950af
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-02-13 00:31:34 +0530

    mock data changes

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,2 @@
 import { int, singlestoreTable, text } from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-  id: int("id", { mode: "int" }).primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
