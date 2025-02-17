6548e09772dff118da7b29189142457be7112d1b
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-02-18 03:31:29 +0530

    chevron fixed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,30 +4,28 @@ import { Button } from "~/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-black to-gray-900 p-8 text-amber-50">
-      <div className="w-full max-w-md space-y-8 text-center">
-        <h1 className="mb-8 text-4xl font-bold">Dhruv&apos;s Personal Drive</h1>
-        <form
-          action={async () => {
-            "use server";
-            const session = await auth();
-            if (!session.userId) {
-              return redirect("/sign-in");
-            }
-            return redirect("/drive");
-          }}
+    <div className="w-full max-w-md space-y-8 text-center">
+      <h1 className="mb-8 text-4xl font-bold">Dhruv&apos;s Personal Drive</h1>
+      <form
+        action={async () => {
+          "use server";
+          const session = await auth();
+          if (!session.userId) {
+            return redirect("/sign-in");
+          }
+          return redirect("/drive");
+        }}
+      >
+        <p className="pb-8 text-lg font-semibold text-red-500">
+          Turn Back If You Are Not Me
+        </p>
+        <Button
+          className="bg-amber-50 text-black transition-colors hover:bg-amber-100"
+          type="submit"
         >
-          <p className="pb-8 text-lg font-semibold text-red-500">
-            Turn Back If You Are Not Me
-          </p>
-          <Button
-            className="bg-amber-50 text-black transition-colors hover:bg-amber-100"
-            type="submit"
-          >
-            Sign In
-          </Button>
-        </form>
-      </div>
-    </main>
+          Sign In
+        </Button>
+      </form>
+    </div>
   );
 }
